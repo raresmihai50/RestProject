fbd20aebbe5819842d0bac6a4bc3800bb9dada88
raresmihai50 <raresmihai50@gmail.com>
2026-03-19 19:13:52 +0200

    database set up and working

--- app/src/main/java/restproject/config/SecurityConfig.java
+++ app/src/main/java/restproject/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -14,6 +16,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- app/src/main/java/restproject/services/UserService.java
+++ app/src/main/java/restproject/services/UserService.java
@@ -1,10 +1,12 @@
 package restproject.services;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import restproject.domain.User;
 import restproject.repo.UserRepository;
 
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
